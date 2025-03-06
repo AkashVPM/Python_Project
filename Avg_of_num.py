c6822679d8c406ec4def21db4f9b0833c1d5fc06
akash <akashvpm440@gmail.com>
2025-03-07 00:24:41 +0530

    Session_2

---
+++ Avg_of_num.py
@@ -0,0 +1,6 @@
+# find average of any number, by Akash S
+def avg (*a):
+      print (sum(a) / len(a)) 
+
+avg (1,2,3,4,5,6,7,8)
+avg (*range(1,101))
---
+++ Centroid_of_dimensions.py
@@ -0,0 +1,26 @@
+# Find centroid of any dimensional value by Akash S
+
+point = []
+center = []
+def centroid (a):
+      print(len(a))
+      b = max(a, key=len)
+      # print(b)
+      for m in range(len(a)):
+            if len(a[m]) <= len(b):
+                  for x in range (len(a[m]),len(b)):
+                        a[m].append(0)
+                  point.append(a[m])
+      print(point)
+      for m in range (len(point[0])):
+            value = 0
+            for n in range(len(point)):
+                  value += point[n][m]
+            average = value / len(a)
+            print(average)
+            center.append(average)
+      print(center)
+
+a = [[1,2,3,4,5,6],[1,2],[1,2,3,4],[5,6],[7],[8,9,10]]
+#[3.8333333333333335, 3.5, 2.6666666666666665, 1.3333333333333333, 0.8333333333333334, 1.0]
+centroid (a)      
