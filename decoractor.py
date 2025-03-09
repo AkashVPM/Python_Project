cad01f855b758eec3c59390248caa7c5956a9d36
akash <akashvpm440@gmail.com>
2025-03-09 22:03:25 +0530

    Change

--- decoractor.py
+++ decoractor.py
@@ -5,25 +5,28 @@ def clock (func):
       def warp ():
             s_t = time.time()   # s_t = start_time 
             res = func()
-            # time.sleep (2)
             e_t = time.time()   # e_t = End_time
-            return f"The time to run the function is {(e_t - s_t):.2f} seconds"
+            return (f"runstart time is {s_t} run end time is {e_t} The time to run the function is {(e_t - s_t)} seconds")
       return warp
 
 def log_call (func):
       def warp():
-            print (func())
-            print(logging.info(f"Function {func.__name__} was called"))
-            # num = func ()
-            return func()
+            res = func()
+            log = logging.info(f"Function {func.__name__} was called")
+            return log
       return warp
 
 @clock
 @log_call
+
 def fun ():
       file = open ("Actuator_Body.stl", "r")
-      # print(file.read())
       return file.read ()
-      # return 5
+print (fun())
 
-print(fun())
+
+# def fun (file):
+#       with open (file, "r")as file:
+#             return file.read ()
+
+# print (fun("Actuator_Body.stl"))
