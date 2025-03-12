7fc8bfad1a8d13f0b1b76b3cc3d813b0d71eeefa
akash <akashvpm440@gmail.com>
2025-03-12 23:20:38 +0530

    change

--- Triangle.py
+++ Triangle.py
@@ -1,51 +1,25 @@
-# Author : Akash S
-# Barycentric_method : https://en.wikipedia.org/wiki/Barycentric_coordinate_system 
-
 import random
 
 def triangle (a,b,c):
-      r1,r2,r3 = random.random(), random.random(), random.random()
-      if r1 + r2 + r3  == 1 :
-            r1 = 1-r2-r3
-            r2 = 1-r3-r1
-            r3 = 1-r2-r1
-      p = a,b,c 
-      [x1,y1,z1], [x2,y2,z2], [x3,y3,z3] = p 
-      print (p)
-
-      point_1 = [ r1*x1 + r2*x2 + r3*x3]
-      point_2 = [ r1*y1 + r2*y2 + r3*y3]
-      point_3 = [ r1*z1 + r2*z2 + r3*z3]
-
-      yield (point_1,point_2,point_3)
-          
-fun = triangle ([1,0,0],[0,1,0],[0,0,1])
-print (next(fun))
-
-     
-
-# import random
-
-# def triangle (a,b,c):
-#     while True:
-#         r1 = random.random()
-#         r2 = (1-r1)*random.random()
-#         r3 = 1-r1-r2
-#         if r1 + r2 + r3  == 1 :
+    while True:
+        r1 = random.random()
+        r2 = (1-r1)*random.random()
+        r3 = 1-r1-r2
+        if r1 + r2 + r3  == 1 :
             
-#             p = a,b,c 
-#             [x1,y1,z1], [x2,y2,z2], [x3,y3,z3] = p 
-#             #print (p)
+            p = a,b,c 
+            [x1,y1,z1], [x2,y2,z2], [x3,y3,z3] = p 
+            #print (p)
 
-#             point_1 =  r1*x1 + r2*x2 + r3*x3
-#             point_2 =  r1*y1 + r2*y2 + r3*y3
-#             point_3 =  r1*z1 + r2*z2 + r3*z3
+            point_1 =  r1*x1 + r2*x2 + r3*x3
+            point_2 =  r1*y1 + r2*y2 + r3*y3
+            point_3 =  r1*z1 + r2*z2 + r3*z3
 
-#             yield [point_1,point_2,point_3]
+            yield [point_1,point_2,point_3]
           
-# fun = triangle ([1,0,0],[0,1,0],[1,1,0])
+fun = triangle ([1,0,0],[0,1,0],[1,1,0])
 
-# with open ("D:/tria_poins.csv","w") as f :
-#     for i in range (100):
-#         p = next(fun)
-#         f.write(str(p[0])+ ',' +str(p[1])+ ','+str(p[2]) +'\n')
+with open ("C:/Akash S/tria_poins.csv","w") as f :
+    for i in range (100):
+        p = next(fun)
+        f.write(str(p[0])+ ',' +str(p[1])+ ','+str(p[2]) +'\n')
--- non_convex_rectangle.py
+++ non_convex_rectangle.py
@@ -1,30 +1,25 @@
 import random
 
-def triangle (a,b,c):
-    while True:
-        r1 = random.random()
-        r2 = (1-r1)*random.random()
-        r3 = 1-r1-r2
-        if r1 + r2 + r3  == 1 :
-            
-            p = a,b,c 
-            [x1,y1,z1], [x2,y2,z2], [x3,y3,z3] = p 
-            #print (p)
+def rectangle (a,b,c,d):
+      def triangle (a,b,c):
+            while True:
+                  r1 = random.random()
+                  r2 = (1-r1)*random.random()
+                  r3 = 1-r1-r2
+                  if r1 + r2 + r3  == 1 :
+                        
+                        p = a,b,c 
+                        [x1,y1,z1], [x2,y2,z2], [x3,y3,z3] = p 
 
-            point_1 =  r1*x1 + r2*x2 + r3*x3
-            point_2 =  r1*y1 + r2*y2 + r3*y3
-            point_3 =  r1*z1 + r2*z2 + r3*z3
+                        point_1 =  r1*x1 + r2*x2 + r3*x3
+                        point_2 =  r1*y1 + r2*y2 + r3*y3
+                        point_3 =  r1*z1 + r2*z2 + r3*z3
 
-            yield [point_1,point_2,point_3]
+                        yield [point_1,point_2,point_3]
 
-Po = [1,0,0],[0,1,0],[1,1,0], [0,0,0]   
-fun_1 = triangle (Po[0],Po[1],Po[2])
-fun_2 = triangle (Po[0],Po[2],Po[3])
-
+fun = rectangle ([1,0,0],[0,1,0],[1,1,0], [0,0,0])   
 
 with open ("C:/Akash S/Rectangle_poins.csv","w") as f :
     for i in range (10000):
-        p = next(fun_1)
-        q = next(fun_2)
-        f.write(str(p[0])+ ',' +str(p[1])+ ','+
-         str(q[0])+ ',' +str(q[1])+ ','+str(p[2]) + ','+str(q[2])+'\n')
+        p = next(fun)
+        f.write(str(p[0])+ ',' +str(p[1])+ ','+ str(q[0])+ ',' +'\n')
