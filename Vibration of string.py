dae94a8d97740b12ab99babcfefa2269410d6956
akash <akashvpm440@gmail.com>
2025-03-21 00:37:12 +0530

    change

--- Vibration of string.py
+++ Vibration of string.py
@@ -1,24 +1,30 @@
-from matplotlib.figure import Figure
+import numpy as np
 import matplotlib.pyplot as plt
 
-dx = 0.1
-dt = 0.01
+dx = 0.01  # Spatial step
+dt = 0.0005  # Time step
+L = 1.0  # Length of string
 
-u_time = []
-u_pos = []
+u_time = np.zeros((0, 3))  
+extent = np.linspace(0, L)
 
+N = int(1/0.01)
 
-
-
+plt.figure()
 for i in range(10000000):
+    for j in range(1,N):
+        u_time[j, 2] = ((u_time[j + 1, 1] - 2 * u_time[j, 1] + u_time[j - 1, 1]) * dt ** 2 / dx ** 2) + 2 * u_time[j, 1] - u_time[j, 0]
     
+    u_time[0, 2] = 0
+    u_time[-1, 2] = 0
     
- 
-    if(i%50 == 1):
-    #if True:
+    u_time[:, 0], u_time[:, 1] = u_time[:, 1], u_time[:, 2]
+
+    if i % 50 == 1:
         plt.clf()
-        plt.plot(extent, unext)
+        plt.plot(extent, u_time[:, 1])
         plt.xlim(-0.1, 1.1)
         plt.ylim(-1.0, 1.0)
         plt.pause(0.001)
+
 plt.show()
