347e46b7058dd134ef29214bf79a13eb854813b2
akash <akashvpm440@gmail.com>
2025-03-24 23:59:28 +0530

    change

---
+++ Dummy.py
@@ -0,0 +1,3 @@
+last_index = [1,2,3]
+last_index = last_index [1:] +last_index [:1]
+print (last_index)
--- Game.py
+++ Game.py
@@ -2,16 +2,26 @@ from hwx import gui
 from hwx.gui.XyPlot import XyPlot
 import random 
 
+# random function to create obstacle 
 number = [ i for i in range (10)] 
 toss = random.choice(number)
 print (toss)
 
+# Buttons layout
 upper_index = [gui.Button() for i in range (10)]
 last_index =[gui.Button() for i in range (10)] 
 last_index [1] = gui.Button(icon="msobjbrowser.png",)
+
+#obstacle creation for game
 if toss % 3 == 0:
       last_index [-1] = gui.Button(icon= "glyphDeleteStrip-16.png",)
 
+# make time function to move the boxeS
+
+# move_box = last_index [1:] +last_index [:1]
+# for i in range (1000):
+#       x = inspire.time(500)
+#       x.connect (move_box)
 
 layout = gui.VFrame(
       gui.HFrame (upper_index),
